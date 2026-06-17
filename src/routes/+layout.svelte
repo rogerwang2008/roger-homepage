7f280fdc81b73ae830b3601c92dd1c408f65ee28
rogerwang2008 <rogerwang2008@163.com>
2026-06-17 23:53:55 +0800

    Add 备案

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,7 @@
 	<footer class="footer footer-center p-10">
 		<aside>
 			<p>汪楚钧 Roger Wang</p>
+			<p><a href="https://beian.miit.gov.cn/" target="_blank">京 ICP 备 2025110603 号-1</a></p>
 		</aside>
 	</footer>
 </div>
