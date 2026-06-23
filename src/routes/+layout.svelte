a1ceb856f3b05a753beb47aa1d115062b27fb27f
rogerwang2008 <rogerwang2008@163.com>
2026-06-23 20:57:20 +0800

    Mod: extract footer to component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 	import '../app.css';
 
 	let { children } = $props();
@@ -9,12 +10,7 @@
 	<Header />
 	<main class="flex-1 flex flex-col w-full">{@render children()}</main>
 
-	<footer class="footer footer-center p-10">
-		<aside>
-			<p>汪楚钧 Roger Wang</p>
-			<p><a href="https://beian.miit.gov.cn/" target="_blank">京 ICP 备 2025110603 号-1</a></p>
-		</aside>
-	</footer>
+	<Footer />
 </div>
 
 <style>
