ccd6417075723a193f3bc7025324f63ecdd5359b
rogerwang2008 <rogerwang2008@163.com>
2026-07-21 13:12:07 +0800

    Update 备案

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,6 +4,6 @@
 <footer class="footer footer-center p-10 animate-fade-in-delay-2">
 	<aside>
 		<p>汪楚钧 Roger Wang</p>
-		<p><a href="https://beian.miit.gov.cn/" target="_blank">京 ICP 备 2025110603 号-1</a></p>
+		<p><a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2026044534号</a></p>
 	</aside>
 </footer>
